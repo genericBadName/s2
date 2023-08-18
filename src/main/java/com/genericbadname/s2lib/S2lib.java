cbb9145dda3e7e963c288303aa8a9a2b3933b039
genericBadName <51008606+genericBadName@users.noreply.github.com>
2023-08-17 19:48:49 -0700

    initial commit

---
+++ src/main/java/com/genericbadname/s2lib/S2lib.java
@@ -0,0 +1,15 @@
+package com.genericbadname.s2lib;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class S2lib implements ModInitializer {
+
+    public static final Logger LOGGER = LogManager.getLogger("S2lib");
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/genericbadname/s2lib/client/S2libClient.java
@@ -0,0 +1,11 @@
+package com.genericbadname.s2lib.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class S2libClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
