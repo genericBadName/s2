2e061f39f74c4fc7dc0562dbb9788546c88e931b
genericBadName <51008606+genericBadName@users.noreply.github.com>
2024-12-08 19:50:03 -0800

    bump to 0.4

---
+++ src/main/java/com/genericbadname/s2lib/data/ModDataGenerator.java
@@ -0,0 +1,14 @@
+package com.genericbadname.s2lib.data;
+
+import com.genericbadname.s2lib.data.provider.ModBlockTagDataProvider;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+        pack.addProvider(ModBlockTagDataProvider::new);
+    }
+}
---
+++ src/main/java/com/genericbadname/s2lib/data/provider/ModBlockTagDataProvider.java
@@ -0,0 +1,20 @@
+package com.genericbadname.s2lib.data.provider;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.level.block.Block;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagDataProvider extends FabricTagProvider<Block> {
+    public ModBlockTagDataProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, Registries.BLOCK, registriesFuture);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider arg) {
+
+    }
+}
